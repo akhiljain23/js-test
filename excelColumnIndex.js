199c387e3185a1690c12a973b370949990941be8
Akhil <akhiljain32@gmail.com>
2024-04-24 16:09:49 +0100

    Update excelColumnIndex.js

--- excelColumnIndex.js
+++ excelColumnIndex.js
@@ -2,7 +2,7 @@
 function solution(columnName) {
   var result = 0;
   for (var i = 0; i < columnName.length; i++) {
-  	/* console.log(columnName.charCodeAt(i)) */;
+    /* console.log(columnName.charCodeAt(i)) */
     /* console.log(result) */
     result = result * 26 + columnName.charCodeAt(i) - 64;
   }
